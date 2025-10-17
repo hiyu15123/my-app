1e667834b6f656f8be5ff66717322c99731edfbc
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-10-17 18:12:50 +0900

    ✨ ヘッダーの追加

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/components/Header.module.css";
+
+export function Header() {
+   return (
+      <header className={styles.header}>
+         <Link href="/" className={styles.anchor}>
+            Page
+         </Link>
+         <Link href="/about" className={styles.anchor}>
+            About
+         </Link>
+      </header>
+   );
+}
