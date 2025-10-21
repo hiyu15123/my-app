e964711f97ff30c7ce0bb106cd0ba91db720c7bd
Hiyu Yamauchi <130884957+hiyu15123@users.noreply.github.com>
2025-10-21 23:11:55 +0900

    ✨ useStateで配列を扱ってみました。

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,12 @@
 import React, { use, useCallback } from "react";
 import { useState } from "react";
 
+
 export function Button() {
    const [count, setCount] = useState(1);
    const [text, setText] = useState("");
    const [isShow, setIsShow] = useState(true);
+   const [array, setArray] = useState<string[]>([]);
 
    const handleClick = useCallback(() => {
       if (count < 10) {
@@ -19,12 +21,22 @@ export function Button() {
          alert("5文字以下にしてください");
          return;
       }
-      setText(e.target.value.trim);
+      setText(e.target.value.trim());
    }, []);
 
    const handleDisplay = useCallback(() => {
       setIsShow((prevIsShow) => !prevIsShow);
    }, []);
+
+   const handleAdd = useCallback(() => {
+      setArray((prevArray) => {
+         if (prevArray.some(item => item === text)) {
+            alert("同じ要素がすでに存在します。");
+            return prevArray;
+         }
+         return [...prevArray, text];
+      });
+   }, [text])
 
    return (
       <>
@@ -40,6 +52,12 @@ export function Button() {
             value={text}
             onChange={handleChange}
          />
+         <button onClick={handleAdd}>追加</button>
+         <ul>
+            {array.map(item => {
+               return <li key={item}>{item}</li>
+            })}
+         </ul>
       </>
    )
 }
